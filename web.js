51859b67840b8222375ee2a0af03faea2392ccab
David Lucena <yakumoklesk@yahoo.es>
2013-07-14 11:11:22 +0200

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync( "index.html" );
+var message = data.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( message );
 });
 
 var port = process.env.PORT || 5000;
